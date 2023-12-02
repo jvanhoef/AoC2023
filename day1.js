1d4273e299503281358964a7ed66ef65545dbe1f
jipvanhoef <jipvanhoef@gmail.com>
2023-12-02 01:56:46 +0100

    feat(day1_1)

--- day1.js
+++ day1.js
@@ -1,7 +1,21 @@
 import { readFile } from 'fs';
 
+let count = 0;
+
 readFile('input_data/day1_1.txt', 'utf-8', function(err, data) {
     if (err) throw err;
     let lines = data.split('\n');
-    console.log(lines[10]);
+    lines.forEach(handleClean);
+
+    console.log(count);
 });
+
+function handleClean(line) {
+    const numbers = line.match(/\d/g);
+    if(!numbers) {
+        return count
+    }
+    const cleanNumberStr = numbers[0] + numbers.slice(-1);
+    const cleanNumber = parseInt(cleanNumberStr);
+    count = count + cleanNumber;
+};
