f6590635758d566a740277f43f3f3f436032c155
jipvanhoef <jipvanhoef@gmail.com>
2023-12-02 02:55:10 +0100

    feat(day1_2)

--- day1.js
+++ day1.js
@@ -1,6 +1,7 @@
 import { readFile } from 'fs';
 
 let count = 0;
+let lines = 0;
 
 readFile('input_data/day1_1.txt', 'utf-8', function(err, data) {
     if (err) throw err;
@@ -10,12 +11,41 @@ readFile('input_data/day1_1.txt', 'utf-8', function(err, data) {
     console.log(count);
 });
 
-function handleClean(line) {
-    const numbers = line.match(/\d/g);
-    if(!numbers) {
-        return count
+function convertWordToNumber(word) {
+    const words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    // Check if word is in words
+    if (words.includes(word)) {
+        const index = words.indexOf(word);
+        return numbers[index];
+    } else {
+        return word;
     }
-    const cleanNumberStr = numbers[0] + numbers.slice(-1);
+
+}
+
+function handleClean(line) {
+    const regex = /one|two|three|four|five|six|seven|eight|nine|\d/g;
+    let regexes = {
+        "one": "o1e",
+        "two": "t2o",
+        "three": "thr3e",
+        "four": "f4or",
+        "five": "fi5ve",
+        "six": "s6x",
+        "seven": "s7ven",
+        "eight": "ei8ght",
+        "nine": "n9ne"
+      };
+      for (let word in regexes) {
+        let regex = new RegExp(word, "g");
+        line = line.replace(regex, regexes[word]);
+      }
+    const numbers = line.match(/\d/g);
+    const cleanNumberStr = convertWordToNumber(numbers[0]) + convertWordToNumber(numbers[numbers.length - 1]);
     const cleanNumber = parseInt(cleanNumberStr);
+    //console.log(cleanNumber, convertWordToNumber(numbers[0]), convertWordToNumber(numbers[numbers.length - 1]), count, lines);
     count = count + cleanNumber;
+    lines++;
+    //console.log(lines);
 };
