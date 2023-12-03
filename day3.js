4dd6ac74f676bb80e8d5c0032b988974a41f6b97
jipvanhoef <jipvanhoef@gmail.com>
2023-12-03 22:55:43 +0100

    wip(day3_1)

---
+++ day3.js
@@ -0,0 +1,38 @@
+import { readFile } from 'fs';
+
+let part1 = 0;
+let part2 = 0;
+let lineCount = 0;
+
+/**
+ * Load data, remove commas and semicolons and split per line
+ */
+readFile('input_data/day2_1.txt', 'utf-8', function(err, data) {
+    // Check for error in data
+    if (err) throw err;
+    //remove unwanted characters and split per line
+    let lines = data.split('\n');
+
+    // Compute solution for part 1 and 2
+    for ( let i = 0; i<lines.length; i++){
+        handlePart1(lines[i], lines[i-1], lines[i-2]);
+    }
+    lines.forEach(handlePart2);
+
+    console.log("part 1: ", part1, "part 2: ", part2);
+});
+
+function handlePart1 (line, previousLine, nextLine) {
+    let characters = line.split('');
+    let previousCharacters = previousLine.split('');
+    let nextCharacters = nextLine.split('');
+    for( let i = 0; i < words.length; i++) {
+        
+    }
+    console.log(characters, previousCharacters, nextCharacters);
+};
+
+
+function handlePart2 (line) {
+    return(part2);
+};
