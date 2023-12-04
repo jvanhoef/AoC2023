fe7420a453371f1f13e009e27d33801ef63d917c
jipvanhoef <jipvanhoef@gmail.com>
2023-12-04 20:16:54 +0100

    wip(day3_1)

--- day3.js
+++ day3.js
@@ -7,15 +7,15 @@ let lineCount = 0;
 /**
  * Load data, remove commas and semicolons and split per line
  */
-readFile('input_data/day2_1.txt', 'utf-8', function(err, data) {
+readFile('input_data/day3_1.txt', 'utf-8', function(err, data) {
     // Check for error in data
     if (err) throw err;
     //remove unwanted characters and split per line
     let lines = data.split('\n');
 
     // Compute solution for part 1 and 2
-    for ( let i = 0; i<lines.length; i++){
-        handlePart1(lines[i], lines[i-1], lines[i-2]);
+    for ( let i = 1; i<lines.length-1; i++){
+        handlePart1(lines[i], lines[i-1], lines[i+1]);
     }
     lines.forEach(handlePart2);
 
@@ -26,8 +26,8 @@ function handlePart1 (line, previousLine, nextLine) {
     let characters = line.split('');
     let previousCharacters = previousLine.split('');
     let nextCharacters = nextLine.split('');
-    for( let i = 0; i < words.length; i++) {
-        
+    for( let i = 1; i < characters.length-1; i++) {
+        if(characters[i])
     }
     console.log(characters, previousCharacters, nextCharacters);
 };
